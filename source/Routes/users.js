aa26957cb5e6d1fe0c6f38e780f412a0359c05ad
vigneshdevelopr <vigneshtpr99@gmail.com>
2023-07-07 23:39:04 +0530

    Auth Complete

--- source/Routes/users.js
+++ source/Routes/users.js
@@ -5,8 +5,14 @@ import { userModel } from '../../source/Models/users.js';
 
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.status(200).send('Data has successfully fetched from userServer');
+router.get('/', async(req,res)=>{
+  try {
+    const allUsers = await userModel.find();
+    console.log(allUsers);
+  return res.status(200).json({allUsers});
+  } catch (error) {
+    return res.status(500).json("Internal Server Error");
+  }
 })
 
 router.post('/signup',async(req, res)=>{
@@ -17,8 +23,8 @@ try {
         return res.status(409).json('Hey Dood, This User is Already Exists')
     }else{
         //hashing the password: 
-
-        const hashPassword = await bcrpyt.hash(password,10)
+const salt = await bcrpyt.genSalt(10)
+        const hashPassword = await bcrpyt.hash(password,salt)
         console.log("hashPassword",hashPassword);
 
         const newUser = new userModel({
@@ -36,7 +42,7 @@ try {
 }
 })
 
-router.post('/login',async(req, res)=>{
+router.post('/signin',async(req, res)=>{
 const{username, password}=req.body;
 try {
     const user = await userModel.findOne({username});
@@ -50,8 +56,9 @@ try {
     }
     //generate Token:
     const genToken = jwt.sign({id: user._id},process.env.SecretKey);
+    return res.status(200).json({token: genToken, userID: user._id});
 } catch (error) {
-    
+    return res.status(500).json('Internal Server Error during Login process')
 }
 })
 
