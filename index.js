ab34d24a569270494dcd591dc36dc8c41ee3895b
vigneshdevelopr <vigneshtpr99@gmail.com>
2023-09-02 11:38:33 +0530

    refresh error fix

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import cors from 'cors';
 import { createConnection } from './Database/Mongodb.js';
 import { userRouter } from './Routes/users.js';
 import { recipeRouter } from './Routes/Recipes.js';
-import path from 'path'
 dotenv.config();
 
 
@@ -16,7 +15,6 @@ const PORT = process.env.port
 //middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'build')));
 app.use('/users',userRouter)
 app.use('/recipes', recipeRouter);
 
@@ -26,9 +24,6 @@ createConnection();
 
 app.get('/',(req,res)=>{
     return res.status(200).json({message:"The Backend Server Database for Cookzzie Recipe Application"})
-})
-app.get('/*', function(req,res) {
-    return res.sendFile(path.join(__dirname,'build','index.html'))
 })
 
 //listening:
